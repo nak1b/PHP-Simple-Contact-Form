f965243487402b85673ef76dcaeb2941c20ee954
Nakib Momin <nbmomin14@gmail.com>
2015-03-31 05:08:17 -0400

    added php code to submit and mail form values

--- index.php
+++ index.php
@@ -1,3 +1,39 @@
+<?php 
+
+	if($_POST['submit']){
+		$result = '<div class="alert alert-success">Form Submitted</div>';
+
+		if(!$_POST['name']){
+			$error = "<br> Please enter your name";
+		}
+
+		if(!$_POST['email']){
+			$error .= "<br> Please enter your email";
+		}
+
+		if(!$_POST['msg']){
+			$error .= "<br> Please enter your message";
+		}
+
+		if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) AND $_POST['email']) { 
+    		$error .= "<br> Please enter a valid email address";
+		} 
+
+		if($error){
+			$result = '<div class="alert alert-danger"> <strong>Therer were error in your form </strong><br>' .$error .'</div>';
+		}
+		else{
+			if(mail("nbmomin14@gmail.com", "Message From Website", "Message")){
+				$result = '<div class="alert alert-success">Thank You!</div>';
+
+			}
+		}
+	}
+
+?>
+
+
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -12,6 +48,9 @@
 		<div class="row">
 			<div class="col-md-6 col-md-offset-3" id="contact-form">
 				<h1>Contact Form</h1>
+
+				<?php echo $result; ?>
+
 				<p class="lead">Please get in touch - i'll get back to yo as soon as i can</p>
 				<form method="POST">
 					<div class="form-group">
@@ -26,10 +65,10 @@
 
 					<div class="form-group">
 						<label for="message">Message:</label>
-						<textarea class="form-control" rows="3"></textarea>
+						<textarea class="form-control" rows="3" name="msg"></textarea>
 					</div>
 					
-					<input type="submit" class="btn btn-success btn-lg" value="submit">
+					<input type="submit" name="submit" class="btn btn-success btn-lg" value="submit">
 				</form>
 
 
