f13a0f1fd8ea6b6d6caed0cdc55ebbb29aeb08f8
Nakib Momin <nbmomin14@gmail.com>
2015-03-31 17:19:40 -0400

    Fixed errors and security issues

--- index.php
+++ index.php
@@ -1,31 +1,48 @@
 <?php 
 
-	if($_POST['submit']){
-		$result = '<div class="alert alert-success">Form Submitted</div>';
+	if(isset($_POST['submit'])){
 
-		if(!$_POST['name']){
-			$error = "<br> Please enter your name";
+		//getting submitted values
+		$name = htmlspecialchars(trim($_POST['name']));
+		$email = htmlspecialchars(trim($_POST['email']));
+		$msg = htmlspecialchars(trim($_POST['msg']));
+
+		$result = '<div class="alert alert-success">Form Submitted</div>';
+		$error = "";
+
+		//checking if name, email and message are entered
+		if(!$name){
+			$error .= "<br> Please enter your name";
 		}
 
-		if(!$_POST['email']){
+		if(!$email){
 			$error .= "<br> Please enter your email";
 		}
 
-		if(!$_POST['msg']){
+		if(!$msg){
 			$error .= "<br> Please enter your message";
 		}
 
-		if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) AND $_POST['email']) { 
+		//checking if valid email
+		if (!filter_var($email, FILTER_VALIDATE_EMAIL) AND $email) { 
     		$error .= "<br> Please enter a valid email address";
 		} 
 
-		if($error){
+		if(!empty($error)){
 			$result = '<div class="alert alert-danger"> <strong>Therer were error in your form </strong><br>' .$error .'</div>';
 		}
 		else{
-			if(mail("nbmomin14@gmail.com", "Message From Website", "Message")){
+
+			//constructing msg to send
+			$emailMsg = "Name: " .$name ."<br> Email: " .$email ."<br> Message: " .$msg;
+
+			//mailing the message
+			if(mail("nbmomin14@gmail.com", "Message From Website", $emailMsg)){
 				$result = '<div class="alert alert-success">Thank You!</div>';
 
+			}
+			else{
+				$result = '<div class="alert alert-danger"> There was errror sending your message</div>';
 			}
 		}
 	}
@@ -49,23 +66,23 @@
 			<div class="col-md-6 col-md-offset-3" id="contact-form">
 				<h1>Contact Form</h1>
 
-				<?php echo $result; ?>
+				<?php if(isset($result)) echo $result; ?>
 
 				<p class="lead">Please get in touch - i'll get back to yo as soon as i can</p>
 				<form method="POST">
 					<div class="form-group">
 						<label for="name">Name:</label>
-						<input type="text" name="name" class="form-control" placeholder="Your Name">
+						<input type="text" name="name" class="form-control" placeholder="Your Name" value="<?php if(isset($name)) echo $name; ?>">
 					</div>
 
 					<div class="form-group">
 						<label for="name">Email:</label>
-						<input type="email" name="email" class="form-control" placeholder="Your Email">
+						<input type="email" name="email" class="form-control" placeholder="Your Email" value="<?php if(isset($email)) echo $email; ?>">
 					</div>
 
 					<div class="form-group">
 						<label for="message">Message:</label>
-						<textarea class="form-control" rows="3" name="msg"></textarea>
+						<textarea class="form-control" rows="3" name="msg"><?php if(isset($msg)) echo $msg; ?></textarea>
 					</div>
 					
 					<input type="submit" name="submit" class="btn btn-success btn-lg" value="submit">
